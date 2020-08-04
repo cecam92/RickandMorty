77998f8f227eae52c4ef997cf2dd14c30565d526
Cesar Martinez <cecam92@hotmail.com>
2020-08-04 22:56:16 +1000

    after install gh-pages and modify package.json

--- src/components/ListOfCharacters/index.jsx
+++ src/components/ListOfCharacters/index.jsx
@@ -7,46 +7,55 @@ function ListOfCaracters() {
   const [page, setPage] = useState(1);
   const api = `https://rickandmortyapi.com/api/character/?page=${page}`;
   const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(null);
   useEffect(() => {
-    fetch(api)
+    fetchData(api);
+  }, [api]);
+
+  function fetchData(url) {
+    setLoading(true);
+    fetch(url)
       .then((res) => res.json())
       .then((response) => {
         setCharacters((characters) => characters.concat(response.results));
       });
-  }, [api]);
-
-  return (
-    <Fragment>
-      <main>
-        <ul className="listCharacters">
-          {" "}
-          {console.log(characters)}
-          {characters.map((character) => (
-            <li className="characters" key={character.id}>
-              <CharacterCard character={character} />
-            </li>
-          ))}
-          {/* <p
+    setLoading(false);
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+      <Fragment>
+        <main>
+          <ul className="listCharacters">
+            {console.log(characters)}
+            {characters.map((character) => (
+              <li className="characters" key={character.id}>
+                <CharacterCard character={character} />
+              </li>
+            ))}
+            {/* <p
             onClick={() => {
               setPage(page + 1);
             }}
           >
             sddadas
           </p> */}
-          {window.addEventListener("scroll", () => {
-            if (
-              window.innerHeight + document.documentElement.scrollTop !==
-              document.documentElement.offsetHeight
-            ) {
-              return;
-            } else {
-              setPage(page + 1);
-            }
-          })}
-        </ul>
-      </main>
-    </Fragment>
-  );
+            {window.addEventListener("scroll", () => {
+              if (
+                window.innerHeight + document.documentElement.scrollTop !==
+                document.documentElement.offsetHeight
+              ) {
+                return;
+              } else {
+                setPage(page + 1);
+              }
+            })}
+          </ul>
+        </main>
+      </Fragment>
+    );
+  }
 }
 
 export default ListOfCaracters;
