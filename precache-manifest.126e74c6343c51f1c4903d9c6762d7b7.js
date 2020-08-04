b37a2980c88fc9d8067347f4b491bf75a167397a
Cesar Martinez <cecam92@hotmail.com>
2020-08-04 22:57:15 +1000

    Updates

---
+++ precache-manifest.126e74c6343c51f1c4903d9c6762d7b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ee627541da10aadb3997cc17fb289a",
+    "url": "/RickandMorty/index.html"
+  },
+  {
+    "revision": "068c8133bc9b3bf61cc8",
+    "url": "/RickandMorty/static/css/main.26a34ba7.chunk.css"
+  },
+  {
+    "revision": "8ea9839d807afb93dd26",
+    "url": "/RickandMorty/static/js/2.51a7c93b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RickandMorty/static/js/2.51a7c93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068c8133bc9b3bf61cc8",
+    "url": "/RickandMorty/static/js/main.9e3cc440.chunk.js"
+  },
+  {
+    "revision": "f8b564e0b69dd2882181",
+    "url": "/RickandMorty/static/js/runtime-main.89bc439a.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/RickandMorty/static/media/HeroImage.42d6efdb.png"
+  }
+]);
