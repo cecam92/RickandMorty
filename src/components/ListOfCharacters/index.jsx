f5455ca763ed1105f4ac7268f153c75e43054567
Cesar Martinez <cecam92@hotmail.com>
2020-08-06 20:29:42 +1000

    save in local

--- src/components/ListOfCharacters/index.jsx
+++ src/components/ListOfCharacters/index.jsx
@@ -4,11 +4,33 @@ import CharacterCard from "../CharacterCard";
 import { Link } from "react-router-dom";
 //import useFetchData from "../hooks/useFetchData";
 
+function saveInLocal(cards, index) {
+  localStorage.setItem("characters", JSON.stringify(cards));
+  localStorage.setItem("page", index);
+}
+function getCharacters() {
+  const pepito = localStorage.getItem("characters");
+  const data = JSON.parse(pepito);
+  return data;
+}
+function getPage() {
+  const index = localStorage.getItem("page");
+  return index;
+}
+
 function ListOfCaracters() {
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(2);
+  const localCharacters = () => {
+    return getCharacters() == null ? [] : getCharacters();
+  };
+
+  const index = () => {
+    return getPage() == null ? 1 : parseInt(getPage());
+  };
+
+  const [page, setPage] = useState(index);
+  const [totalPages, setTotalPages] = useState(28790);
   const api = `https://rickandmortyapi.com/api/character/?page=${page}`;
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState(localCharacters);
 
   function isScrolling() {
     if (
@@ -22,14 +44,17 @@ function ListOfCaracters() {
       }
     }
   }
+
   useEffect(() => {
     fetchData(api);
+    saveInLocal(characters, page);
     window.addEventListener("scroll", isScrolling);
-    return () => window.removeEventListener("scroll", isScrolling);
+    return () => {
+      window.removeEventListener("scroll", isScrolling);
+    };
   }, [api, page]);
 
   function fetchData(url) {
-    // console.log(url);
     fetch(url)
       .then((res) => res.json())
       .then((response) => {
@@ -54,13 +79,6 @@ function ListOfCaracters() {
               </Link>
             </li>
           ))}
-          {/* <button
-            onClick={() => {
-              setPage(page + 1);
-            }}
-          >
-            sddadas
-          </button> */}
         </ul>
       </main>
     </Fragment>
