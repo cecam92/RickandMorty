b034b2f1af6c211a13307ec8590b246befba608b
Cesar Martinez <cecam92@hotmail.com>
2020-08-07 15:29:06 +1000

    Updates

--- precache-manifest.9075f1dd72837177df1ac93c72f8b524.js
+++ precache-manifest.9075f1dd72837177df1ac93c72f8b524.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d55524b3caefeb5349b602db00ef69a1",
+    "revision": "661f7d95d0a5e8e5f5e89c4c27648815",
     "url": "/RickandMorty/index.html"
   },
   {
-    "revision": "90a3b7e73baf5e5b1e55",
-    "url": "/RickandMorty/static/css/main.e7b64edf.chunk.css"
+    "revision": "ea6fcec52abd10c7e383",
+    "url": "/RickandMorty/static/css/main.fc5b29b1.chunk.css"
   },
   {
-    "revision": "e0374b64d0cc6155e646",
-    "url": "/RickandMorty/static/js/2.eda4ad79.chunk.js"
+    "revision": "1b99111ed810a752599d",
+    "url": "/RickandMorty/static/js/2.6d1a08db.chunk.js"
   },
   {
     "revision": "c87550333da8df8ab4c80bd1d6e9defb",
-    "url": "/RickandMorty/static/js/2.eda4ad79.chunk.js.LICENSE.txt"
+    "url": "/RickandMorty/static/js/2.6d1a08db.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "90a3b7e73baf5e5b1e55",
-    "url": "/RickandMorty/static/js/main.80212b9e.chunk.js"
+    "revision": "ea6fcec52abd10c7e383",
+    "url": "/RickandMorty/static/js/main.d71b6afe.chunk.js"
   },
   {
     "revision": "f8b564e0b69dd2882181",
     "url": "/RickandMorty/static/js/runtime-main.89bc439a.js"
+  },
+  {
+    "revision": "bc95a60c23d35498d0fd68fcf0a9f8d3",
+    "url": "/RickandMorty/static/media/Buried.bc95a60c.jpeg"
   },
   {
     "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RickandMorty/precache-manifest.da9dd0942c6ed637e9cb000557c6db78.js"
+  "/RickandMorty/precache-manifest.9075f1dd72837177df1ac93c72f8b524.js"
 );
 
 self.addEventListener('message', (event) => {
